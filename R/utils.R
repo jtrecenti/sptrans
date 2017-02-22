eed6e3337f47d5e938729b8c08210e7caa5cbfe2
Julio Trecenti <julio.trecenti@gmail.com>
2017-02-22 14:13:50 -0300

    global vars

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,10 @@
+globalVariables(c(".", "CodigoLinha", "DenominacaoTPTS", "DenominacaoTSTP", "Letreiro", 
+                  "Sentido", "Tipo", "", "URLencode", "color", "direction", "distance", 
+                  "linha", "m", "n", "nc", "rainbow", "route_id", "", "shape_id", 
+                  "shapes", "stop_id", "stop_id1", "stop_id2", "stop_sequence", 
+                  "stop_times", "", "stops", "tem1", "tem2", "trip_id", "trips", 
+                  "vale"))
+
 #' Pipe operator
 #'
 #' See \code{\link[magrittr]{\%>\%}} for more details.
